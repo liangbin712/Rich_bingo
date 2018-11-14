f2ef9448efcf5cd81c697501fd14ab0773307f54
liangbin <592469893@qq.com>
2018-11-14 17:21:02 +0800

    Signed-off-by: liangbin <592469893@qq.com> 三子棋2

--- Project11-13/Project11-13/源11-13.c
+++ Project11-13/Project11-13/源11-13.c
@@ -1,219 +1,173 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<windows.h>
-#include<time.h>
-#define ROWS 3
-#define COLS 3
-void menu();
-void init_board(char arr[][COLS], int x, int y);//初始化棋盘
-void print_board(char arr[][COLS], int x, int y);//打印棋盘
-void player_move(char arr[][COLS]);//玩家下棋
-int check_full(char arr[][COLS], int x, int y);//判断棋盘是否已满
-void computer_move(char arr[][COLS]);//电脑下棋
-char check_win(char arr[][COLS], int x, int y);//判断谁赢
-#include"three_chess.h"
-void menu()
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#define Row 3
+#define Col 3
+int g_board[Row][Col];
+void qipan(char g_board[Row][Col])
 {
-	printf("****************\n");
-	printf("****1.play******\n");
-	printf("****0.exit******\n");
-	printf("****************\n");
-}
-
-void init_board(char arr[][COLS], int x, int y)//传数组时一般要把数组的下标也传进来
-{
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < x; i++)
+	for (int row = 0; row < Row; ++row)
 	{
-		for (j = 0; j < y; j++)
+		for (int col = 0; col < col; ++col)
 		{
-			arr[i][j] = ' ';//将二维数组初始化为空格
+			g_board[row][col] = ' ';
 		}
 	}
 }
-
-void print_board(char arr[][COLS], int x, int y)
+void print(char g_board[Row][Col])
 {
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < x; i++)
+	for (int row = 0; row < Row; ++row)
 	{
-		printf("%c |%c |%c\n", arr[i][0], arr[i][1], arr[i][2]);
-
-		if (i < 2)  //一共打印三行棋盘，则在打印第三行时不打印下边的横线
+		printf("| %c | %c | %c |\n", g_board[row][0], g_board[row][1], g_board
+			[row][2]);
+		if (row != Row - 1)
 		{
-			printf("--|--|--\n");
+			printf(" ---|---|----\n");
 		}
 	}
 }
-
-
-void player_move(char arr[][COLS])
+void PlayerMove(char g_board[Row][Col])
 {
-	int x = 0;
-	int y = 0;
+	printf("请玩家落子！\n");
 	while (1)
 	{
-		printf("请输入你要下的坐标>");
-		scanf("%d %d", &x, &y);
-		x--;//数组元素的下标是从0开始，所以需要减一
-		y--;
-
-		if (arr[x][y] == ' ')//如果这个下标下的元素为空格，则将p赋给这个元素
+		printf("请玩家输入坐标(row col):");
+		int row, col;
+		scanf("%d %d", &row, &col);
+		if (row < 0 || row >= Row || col < 0 || col >= Row)
 		{
-			arr[x][y] = 'p';
-			break;//跳出循环
+			printf("输入的非法坐标，请重新输入！\n");
+			continue;
 		}
-		else
+		 if (g_board[row][col] == 'x' || g_board[row][col] == 'o')
 		{
-			printf("你输入的下标已被占，请重新输入\n>");
-			continue;//跳出本次循环
+			printf("当前位置已经有其他子，请重新输入");
+			continue;
 		}
-
+		 g_board[row][col] = 'x';
+		 break;
 	}
 }
-
-
-int check_full(char arr[][COLS], int x, int y)
+void ComputerMove(char g_board[Row][Col])
 {
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < x; i++)
-	{
-		for (j = 0; j < y; j++)
-		{
-			if (arr[i][j] == ' ')//判断数组中有没有空格，如果出现一次空格，就已经可以证明数组没满
-			{
-				return 0;
-			}
-		}
-	}
-	return 1;//如果程序可以执行到这里，说明数组已经满了，则返回0
-}
-
-void computer_move(char arr[][COLS])
-{
-	srand((unsigned int)time(NULL));//为了每次产生不同的随机数
+	printf("请电脑落子\n");
 	while (1)
 	{
-		int x = rand() % 3;//产生一个小于3的数
-		int y = rand() % 3;
-		if (check_full(arr, ROWS, COLS) == 0)//判断棋盘是否已经满了
+		int row = rand() % Row;
+		int col = rand() % Col;
+		if (g_board[row][col] == 'x' || g_board[row][col] == 'o')
 		{
-			if (arr[x][y] == ' ')//判断这个元素是否为空
-			{
-				arr[x][y] = 'c';//如果为空，则赋c给这个元素
-				break;//跳出这层循环
-			}
-			else
-			{
-				continue;//继续执行下一次的循环
-			}
+			continue;
 		}
-
+		g_board[row][col] = 'o';
+		break;
 	}
 }
-
-
-
-char check_win(char arr[][COLS], int x, int y)
+char CheckWinner(char g_board[Row][Col], int row, int col)
 {
-	int i = 0;
-	if (check_full(arr, ROWS, COLS) == 1)//如果棋盘满了，则返回q
+	//检查行，列，斜线是否连成三个
+	//先检查所以行
+	for (int row = 0; row < Row; ++row)
+	{
+		if (g_board[row][0] == g_board[row][1]
+			&& g_board[row][0] == g_board[row][2]
+			&& g_board[row][0] != ' ')
+		{
+			return g_board[row][0];
+		}
+	}
+	//return ' ';
+	//再检查所有列
+	for (int col = 0; col < col; ++col)
+	{
+		if (g_board[0][col] == g_board[1][col]
+			&& g_board[0][col] == g_board[2][col]
+			&& g_board[0][col] != ' ')
+		{
+			return g_board[0][col];
+		}
+	}//检查对角线
+	if (g_board[0][0] == g_board[1][1]
+		&& g_board[0][0] == g_board[2][2]
+		&& g_board[0][0] != ' ')
+	{
+		return g_board[0][0];
+	}
+	if (g_board[2][0] == g_board[1][1]
+		&& g_board[2][0] == g_board[0][2]
+		&& g_board[2][0] != ' ')
+	{
+		return g_board[2][0];
+	}
+	else if (check_full(g_board, row, col))
 	{
 		return 'q';
 	}
-	for (i = 0; i < x; i++)
-	{
-		if ((arr[i][0] == arr[i][1]) && arr[i][1] == arr[i][2])
-		{
-			return arr[i][0];//返回三个元素中的任一个元素
-		}
-	}
-
-	for (i = 0; i < y; i++)
-	{
-		if ((arr[0][i] == arr[1][i]) && arr[1][i] == arr[2][i])
-		{
-			return arr[1][i];
-		}
-	}
-
-	if ((arr[0][0] == arr[1][1]) && arr[1][1] == arr[2][2])
-	{
-		return arr[0][0];
-	}
-
-	if ((arr[0][2] == arr[1][1]) && arr[1][1] == arr[2][0])
-	{
-		return arr[1][1];
-	}
-
-	return ' ';//如果没有判断出谁赢并且棋盘还没满，则继续进行游戏
+	return 0;
 }
-
-
-
-#include"three_chess.h"
-
-void game()//游戏开始
+int check_full(char g_board[Row][Col], int row, int col)
 {
-	char ret = 0;
-	char arr[ROWS][COLS];
-	init_board(arr, ROWS, COLS);//初始化棋盘
-	print_board(arr, ROWS, COLS);//打印棋盘
-	do
+	for (row = 0; row < Row;
+		row++)
 	{
-		player_move(arr);//玩家先走
-		print_board(arr, ROWS, COLS);//打印出棋盘
-		if (check_win(arr, ROWS, COLS) != ' ')//如果棋盘不为空的话，就跳出循环
+		for (col = 0; col < Col; col++)
 		{
+			if
+				(g_board[row][col] == ' ')
+			{
+				return 0;
+
+			}
+		}
+	}
+	return 1;
+}
+int main()
+{
+	srand((unsigned int)time(NULL));
+	//1.先对棋盘进行初始化
+	qipan(g_board, Row, Col);
+	char winner;
+	while (1)
+	{//2.先打印出棋盘
+		print(g_board);
+		//3.玩家落子，判游戏是否结束
+		PlayerMove(g_board);
+		//玩家的子为x，电脑的子为o
+		//winner -> x玩家胜
+		//winner -> o电脑胜
+		//winner -> ‘ ’胜负未分
+		//winner -> q 和棋
+		winner = CheckWinner(g_board, Row, Col);
+		if (winner == 'x' || winner == 'o')
+		{
+			print(g_board);
 			break;
 		}
-		printf("电脑下：\n");
-		computer_move(arr);//否则电脑下棋
-		print_board(arr, ROWS, COLS);//把电脑下过后的棋盘打印出来
-		ret = check_win(arr, ROWS, COLS);//将判断输赢后的返回值保存起来
-	} while (ret == ' ');//如果棋盘没有满，同时也没有判断出输赢的话，继续执行循环，否则跳出循环
-
-	if (check_win(arr, ROWS, COLS) == 'p')//如果判断输赢的返回值为玩家下的字符则就能判断出玩家赢
-	{
-		printf("玩家赢\n");
+		//4.电脑落子，判定游戏是否结束
+		print(g_board);
+		ComputerMove(g_board);
+		winner = CheckWinner(g_board, Row, Col);
+		if (winner == 'x' || winner == 'o')
+		{
+			print(g_board);
+			break;
+		}
 	}
-	else if (check_win(arr, ROWS, COLS) == 'c')//如果判断输赢的返回值为c,则可以判断电脑赢
-
+	if (winner == 'x')
 	{
-		printf("电脑赢\n");
-
+		printf("玩家胜\n");
+	}
+	else if (winner == 'o')
+	{
+		printf("电脑胜\n");
 	}
 	else
 	{
-		if (check_win(arr, ROWS, COLS) == 'q')//如果棋盘下满了，即就是返回值为q的时候，则可以判断为平局
-			printf("平局\n");
+		printf("和棋\n");
 	}
-
+	system("pause");
+	return 0;
 }
-
-
-
-
-int main()
-{
-	int choose = 0;
-	do
-	{
-		menu();
-		printf("请选择>");
-		scanf("%d", &choose);
-		switch (choose)
-		{
-		case 0:
-			exit(1);
-		case 1:
-			game();
-		}
-
-	} while (choose);
-}
-
