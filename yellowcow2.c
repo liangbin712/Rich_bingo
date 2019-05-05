40cd56b97e67e40fcf6d71864e9efe607efb7b42
biu <biu@localhost.localdomain>
2019-05-05 19:00:01 +0800

    yellow cow strcamble for tickes

--- yellowcow2.c
+++ yellowcow2.c
@@ -7,38 +7,42 @@ pthread_mutex_t mutex;
 int ticket = 100;
 void *(yellowcow)(void *i)
 {
-  int *p = (int *)i;
-  while(ticket > 0)
+  while(1)
   {
+    int *p = (int *)i;
     pthread_mutex_lock(&mutex);
+    if(ticket > 0)
+    {
+    usleep(100000);
     printf("[%d]yellow cow take [%d]# ticket\n",p,ticket);
     ticket--;
+    }
+    else{
+      pthread_mutex_unlock(&mutex);
+      pthread_exit(NULL);
+    }
     pthread_mutex_unlock(&mutex);
-    usleep(100000);
   }
-  pthread_exit(0);
 }
 int main()
 {
   int ret;
-  long int i = 4;
+  long int i =0;
   pthread_t tid[4];
-  void **o = 0;
-  while(i > 0)
+  pthread_mutex_init(&mutex,NULL);
+  while(i < 4)
   {
-    ret = pthread_create(&tid[i],NULL,&yellowcow,(void*)i);
-    i--;
+    ret = pthread_create(&tid[i],NULL,yellowcow,(void*)i);
+    i++;
     if(ret != 0)
     {
       printf("yelow cow no exist\n");
       return -1;
     }
   }
-  pthread_mutex_init(&mutex,NULL);
-  i = 0;
-  for(i = 0;i < 4;i++)
+  for(i = 3;i >= 0;i--)
   {
-    pthread_join(tid[i],o);
+    pthread_join(tid[i],NULL);
   }
   pthread_mutex_destroy(&mutex);
   return 0;
